9cd9914e6719a2920672579bc09d9f414b1ad7c8
DKOOK01 <aymansadik2005@gmail.com>
2026-08-17 13:14:19 +0100

    .

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -14,7 +14,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
     # 2. Start MariaDB temporarily in the background
-    mysqld --user=mysql --datadir=/var/lib/mysql &
+    mysqld --user=mysql &
     pid="$!"
     
     # 3. Wait until it is fully started
